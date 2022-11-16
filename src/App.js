b7229cda1d7f3786986f490b9360332ea944e87e
metariqul <metariqul91@gmail.com>
2022-11-16 19:00:21 +0600

    searching and filtering

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
+import { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+
+    constructor() {
+        console.log('constructor');
+        super();
+        this.state = {
+            monstars: [],
+            searchField: '',
+        }
+    }
+
+    componentDidMount() {
+        console.log('component didmount')
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then((response) => response.json())
+            .then((users) => this.setState(() => {
+                return { monstars: users }
+            }, () => {
+                console.log(this.state);
+            }))
+    }
+    render() {
+        console.log('render');
+        const filterMonsters = this.state.monstars.filter( (monster) => {
+            return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+        });
+
+        const dd = this.state.monstars.filter( (monster) => {
+            return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+        });
+        return (
+            
+            <div className="App">
+                <input type="search" className='search-box' placeholder='Search Monster' onChange={ (event) => {
+                    console.log({startingArr: this.state.monstars});
+                    const searchField = event.target.value.toLocaleLowerCase();
+                  
+                    this.setState( () => {
+                        return {searchField}
+                       
+                    }, () => { 
+                        console.log({endingarry: this.state.monstars});
+                    });
+                }} />
+                <header className="App-header">
+                    {filterMonsters.map((monster) => {
+                        return <div key="{monster.id}"><h2>{monster.name}</h2></div>
+                    })}
+
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
